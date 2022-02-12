4a7be7925b0f3903380fba6c896d261b1c794741
Victor Augusto <victor.augusto@discente.univasf.edu.br>
2022-02-12 13:43:31 -0300

    Now could insert events

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -32,7 +32,7 @@ def submit_login(request):
             return redirect('/')
         else:
             messages.error(request, "Usuário ou senha inválidos")
-            
+
     return redirect('/')
 
 @login_required(login_url='/login/') #boto esse atributo dentro do login_required, pra me redirecionar quando n tiver autenticado
@@ -44,3 +44,23 @@ def lista_eventos(request):
     #evento= Evento.objects.all() #pra mostrar todos os eventos
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo=request.POST.get('titulo')
+        data_evento=request.POST.get('data_evento')
+        descricao=request.POST.get('descricao')
+        usuario=request.user
+        Evento.objects.create(titulo=titulo,
+                            data_evento=data_evento,
+                            descricao=descricao,
+                            usuario=usuario
+                            )
+        return redirect('/')
+
+
